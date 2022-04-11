52e02a8108b17ad2591fe09a00f12c0ef8e8a453
Néstor Moreno <ultranestor@hotmail.com>
2022-04-11 14:44:37 -0500

    Se actualizan las clases

---
+++ UberProyect.playground/Sources/UberX.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+class UberX: Car {
+    var brand: String
+    var model: String
+    
+    init(id: Int,
+         license: String,
+         driver: String,
+         passenger: Int,
+         brand: String,
+         model: String){
+        self.brand = brand
+        self.model = model
+        super.init(id: id, license: license, driver: driver, passenger: passenger)
+    }
+}
